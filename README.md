66c7d85d43b223cb39aee4f96e4138ce680ef192
Eero af Heurlin <eero.afheurlin@iki.fi>
2011-12-15 22:25:42 +0200

    stubs based on pca9635RGB code

---
+++ README.md
@@ -0,0 +1,13 @@
+# Library for handling multiple pca9635RGB boards
+
+Try to handle N board with some resemblance of efficiency, boards must
+have sequential base addresses.
+
+## Installing
+
+Clone this repo to your arduino libraries folder, then clone the following repos as well:
+
+  * git://github.com/rambo/I2C.git
+  * git://github.com/rambo/pca9635.git
+  * git://github.com/rambo/i2c_device.git
+  * git://github.com/rambo/pca9635RGB.git
---
+++ pca9635multiRGB.h
@@ -0,0 +1,21 @@
+// safety againts double-include
+#ifndef pca9635RGB_h
+#define pca9635RGB_h
+#include <WProgram.h> 
+#include <pca9635.h>
+#include <pca9635RGB.h>
+
+class pca9635multiRGB : public pca9635RGB
+{
+    public:
+        // Initialize the object and chip        
+        void begin(byte first_board, byte last_board, boolean wire_begin);
+        // A funky way to handle optional arguments
+        void begin(byte first_board, byte last_board);
+        void begin();
+        boolean set_rgb(byte ledno, byte rcycle, byte gcycle, byte bcycle);
+        
+};
+
+#endif
+// *********** END OF CODE **********
